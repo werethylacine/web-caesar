d1846da654f2cde19b7fbbc063c654fe1121bf6f
Claire Condon <werethylacine@gmail.com>
2017-01-23 19:13:18 -0600

    Adjusted alignment, font, added caesar favicon & title

--- main.py
+++ main.py
@@ -21,18 +21,18 @@ import cgi
 def buildpage(text_area_content):
     styles = '''<style>
         * {
-            font-family: "Arial Black", Gadget, sans-serif;
+            font-family: "Trebuchet", Gadget, sans-serif;
             color: #111;
         }
         img {
-            width: 85%;
+            height: 95vh;
         }
         #eyes {
             z-index: -1;
             position: absolute;
             transform: translate3d(-1px, 0px, 0px);
             transition: transform 1s ease-in-out;
-            width: 80%;
+            width: 68%;
         }
         #bust:hover + #eyes{
             transform: translate3d(18px, 0px, 0px);
@@ -41,7 +41,7 @@ def buildpage(text_area_content):
             z-index: +1;
             position: absolute;
             float:left;
-            width: 80%;
+            width: 68%;
         }
         .right {
             float: right;
@@ -56,17 +56,17 @@ def buildpage(text_area_content):
             width: 70px;
         }
         #message {
-            width: 150px;
+            width: 205px;
             height: 120px;
         }
 
     </style>'''
     img_divs = '<div id="bust"><img src = "/images/caesar_bust.png"/></div><div id="eyes"><img src = "/images/caesar_eyes.png"/></div>'
-    header = '<h2 class="right">Caesar Cipher</h2><br>'
-    rot_label = '<label>Rotate by: </label>'
-    rotation = '<input id="rot" type = "number" name = "rot" >'
-    text_label = '<label>Message to encode: </label>'
-    textarea = '<textarea id="message" name = "message">' + text_area_content + '</textarea>'
+    header = '<title>Caesar Cipher</title><h2 class="right">Caesar Cipher</h2><br>'
+    rot_label = '<div class="right"><label>Rotate by: </label>'
+    rotation = '<input id="rot" type = "number" name = "rot" ></div>'
+    text_label = '<div class="right"><label>Message to encode: </label>'
+    textarea = '<textarea id="message" name = "message">' + text_area_content + '</textarea></div>'
     button = '<input class = "right" type="submit" />'
     form = '<div class="right">' + styles + '<form method="post">' + rot_label + rotation + '<br>' + text_label + textarea + '<br>' + button + '</form></div>'
     return header + form + img_divs
